4cb2534eae632d2a0e73030cdccf950b230b2886
Brian Chang <brian.h.chang1@gmail.com>
2017-07-22 20:25:01 -0500

    update

--- custom.py
+++ custom.py
@@ -8,13 +8,13 @@ password = "11478542"
 from_email = username
 to_list = ["b.b.briann@gmail.com"]
 
-email_conn = smtplib.SMTP(host, port)
-email_conn.ehlo()
-email_conn.starttls()
-email_conn.login(username, password)
-email_conn.sendmail(from_email, to_list, "Hello there this is python test2.")
+# email_conn = smtplib.SMTP(host, port)
+# email_conn.ehlo()
+# email_conn.starttls()
+# email_conn.login(username, password)
+# email_conn.sendmail(from_email, to_list, "Hello there this is python test2.")
 
-email_conn.quit()
+# email_conn.quit()
 
 
 # from smtplib import SMTP
@@ -25,3 +25,18 @@ email_conn.quit()
 # email_conn2.starttls()
 # email_conn2.login(username, password)
 # email_conn2.sendmail(from_email, to_list, "Hello there this is python test2.")
+
+from smtplib import SMTPException, SMTP, SMTPAuthenticationError
+
+pass_wrong = SMTP(host, port)
+pass_wrong.ehlo()
+pass_wrong.starttls()
+try:
+    pass_wrong.login(username, "password")
+    pass_wrong.sendmail(from_email, to_list, "Hello there this is python test2.")
+except SMTPAuthenticationError:
+    print("Could not login")
+except:
+    print("an error occured")
+
+pass_wrong.quit()
