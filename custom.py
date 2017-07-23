9d229d9e19803b8516be4706553805306a6c86ca
Brian Chang <brian.h.chang1@gmail.com>
2017-07-22 20:17:27 -0500

    update

---
+++ custom.py
@@ -0,0 +1,27 @@
+import smtplib
+
+host = "smtp.gmail.com"
+port = 587
+username = "sxsw2016bchang@gmail.com"
+password = "11478542"
+
+from_email = username
+to_list = ["b.b.briann@gmail.com"]
+
+email_conn = smtplib.SMTP(host, port)
+email_conn.ehlo()
+email_conn.starttls()
+email_conn.login(username, password)
+email_conn.sendmail(from_email, to_list, "Hello there this is python test2.")
+
+email_conn.quit()
+
+
+# from smtplib import SMTP
+
+# email_conn2 = SMTP(host, port)
+# email_conn2 = smtplib.SMTP(host, port)
+# email_conn2.ehlo()
+# email_conn2.starttls()
+# email_conn2.login(username, password)
+# email_conn2.sendmail(from_email, to_list, "Hello there this is python test2.")
